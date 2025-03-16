f3780e87cccd68b26727f328bc7d5e8cdb99ee15
Md. Sakib Ahamed <sakib.cse072@gmail.com>
2025-03-16 15:29:34 +0600

    scrapper commit

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-# service = Service(executable_path="chrome-linux64/chrome")
 driver = webdriver.Chrome()
-#driver.set_page_load_timeout(300)
 driver.get('https://www.daraz.pk/catalog/?spm=a2a0e.tm80335142.search.d_go&q=sunglasses')
 driver.maximize_window()
 
@@ -17,13 +11,12 @@ image_links = []
 for i in range(1,40):
     j = str(i)
     
-    image_link = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[{j}]/div/div/div[1]/div/a/div/img').get_attribute('src')
-    image_links.append(image_link)
+    image_link = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div['+ j +']/div/div/div[1]/div/a/div/img').get_attribute('src')
+    image_links.append(str(image_link))
+    # print(image_link)
 
-
+#image_link = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[1]/div/a/div/img').get_attribute('src')
 print(image_links)
-
-# WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="searchbox_input"]')))
 
 time.sleep(60)
 driver.quit()
